a2ca9bc360b2e3dca9b7609ec81cb947cdba8b91
Gordon Wells <gordon.wells@gmail.com>
2015-12-22 22:22:53 +0200

    Added rc.py for percol settings

---
+++ rc.py
@@ -0,0 +1,4 @@
+percol.view.CANDIDATES_LINE_BASIC    = ("on_default", "default")
+percol.view.CANDIDATES_LINE_SELECTED = ("underline", "on_blue", "white","bold")
+percol.view.CANDIDATES_LINE_MARKED   = ("bold", "on_cyan", "black")
+percol.view.CANDIDATES_LINE_QUERY    = ("green", "bold")
